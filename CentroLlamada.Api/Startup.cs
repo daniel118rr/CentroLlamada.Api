9d5f19e4bd6bff21e79f91f515bc60eb0e467f00
Daniel Ramirez <daniel118rr@gmail.com>
2021-12-20 15:19:09 -0500

    Commit cambios basicos

--- CentroLlamada.Api/Startup.cs
+++ CentroLlamada.Api/Startup.cs
@@ -34,15 +34,6 @@ namespace CentroLlamada.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -50,7 +41,7 @@ namespace CentroLlamada.Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
