2af32dbdc432c733bb351619c7184bf49ae9d96f
Daniel Ramirez <daniel118rr@gmail.com>
2021-12-21 14:13:38 -0500

    Delete Folder Delete Segv2

--- CentroLlamada.Api/Startup.cs
+++ CentroLlamada.Api/Startup.cs
@@ -22,7 +22,6 @@ namespace CentroLlamada.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
             services.AddControllers();
             services.AddEntity<CentroLlamada.Domain.Paciente, string>();
         }
